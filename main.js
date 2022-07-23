c47a0a01b8ab9c9240c2a6349be162cffb6097d4
wjidhfyu3erf9irhui <103157728+wjidhfyu3erf9irhui@users.noreply.github.com>
2022-07-22 19:45:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+
+
+
